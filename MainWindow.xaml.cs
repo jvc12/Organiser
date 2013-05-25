b5498e5578adf15ffd48af029b7bee13fc637c00
varuncoe <jvc.root@gmail.com>
2013-05-25 17:30:36 +0530

    Update

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace Organiser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            con.Open();
+            //SqlCommand cmd = new SqlCommand("insert into organiser select '" + datePicker1.Text + "','" + textBox1.Text +"'", con);  // Sql query and connection object is assigned to SqlCommand object i.e. ‘cmd’.
+            SqlCommand cmd1 = new SqlCommand("insert into Organizer select '" + DateTimePicker1.Text +"'", con);
+            //cmd.ExecuteNonQuery();   // SqlCommand is executed
+            cmd1.ExecuteNonQuery();
+            MessageBox.Show("Reminder is set Successfully on "+DateTimePicker1.Text); //Show message box
+            con.Close();  // Closes the connection
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)Application.LoadComponent(new Uri("Window1.xaml", UriKind.Relative));
+            window.Show();
+            
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+
+namespace Organiser
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            string ConString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            string CmdString = string.Empty;
+            using (SqlConnection con = new SqlConnection(ConString))
+            {
+                
+                CmdString = "SELECT * FROM Organizer";
+                SqlCommand cmd = new SqlCommand(CmdString, con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable("Organizer");
+                sda.Fill(dt);
+                dataGrid1.ItemsSource = dt.DefaultView;
+            }  
+        }
+    }
+}
